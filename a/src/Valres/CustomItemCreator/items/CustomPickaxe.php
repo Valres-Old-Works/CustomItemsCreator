879725202ee8035eaab9504c70205c6cdae21fff
Valres <tsterle59@gmail.com>
2024-10-10 01:29:20 +0200

    ?

---
+++ a/src/Valres/CustomItemCreator/items/CustomPickaxe.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ *
+ *     ______           __                  ______                 ______                __
+ *   / ____/_  _______/ /_____  ____ ___  /  _/ /____  ____ ___  / ____/_______  ____ _/ /_____  _____
+ *  / /   / / / / ___/ __/ __ \/ __ `__ \ / // __/ _ \/ __ `__ \/ /   / ___/ _ \/ __ `/ __/ __ \/ ___/
+ * / /___/ /_/ (__  ) /_/ /_/ / / / / / // // /_/  __/ / / / / / /___/ /  /  __/ /_/ / /_/ /_/ / /
+ * \____/\__,_/____/\__/\____/_/ /_/ /_/___/\__/\___/_/ /_/ /_/\____/_/   \___/\__,_/\__/\____/_/
+ *
+ * ENG: This file is strictly confidential and personal.
+ * It contains code developed for private purposes and must not be distributed, shared or used without the explicit permission of the author.
+ * Any violation will be subject to legal action.
+ * FRA: Ce fichier est strictement confidentiel et personnel.
+ * Il contient du code développé à des fins privées et ne doit en aucun cas être distribué, partagé ou utilisé sans autorisation explicite de l'auteur.
+ * Toute violation sera passible de poursuites légales.
+ *
+ * @author ValresMC
+ * @version v0.0.1
+ */
+
+declare(strict_types=1);
+
+namespace Valres\CustomItemCreator\items;
+
+use pocketmine\item\ItemIdentifier;
+use pocketmine\item\ItemTypeIds;
+use pocketmine\item\Pickaxe;
+use pocketmine\item\ToolTier;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\ListTag;
+use Valres\CustomItemCreator\libs\item\component\DamageComponent;
+use Valres\CustomItemCreator\libs\item\component\DurabilityComponent;
+use Valres\CustomItemCreator\libs\item\component\HandEquippedComponent;
+use Valres\CustomItemCreator\libs\item\CreativeInventoryInfo as CII;
+use Valres\CustomItemCreator\libs\item\ItemComponents;
+use Valres\CustomItemCreator\libs\item\ItemComponentsTrait;
+use Valres\CustomItemCreator\utils\DiggerList;
+
+final class CustomPickaxe extends Pickaxe implements ItemComponents
+{
+    use ItemComponentsTrait {
+        getComponents as _getComponents;
+    }
+
+    protected int $damage;
+    protected int $durability;
+    protected int $efficiency;
+
+    public function __construct(string $name, string $texture, int $efficiency, int $damage, int $durability) {
+        parent::__construct(
+            new ItemIdentifier(ItemTypeIds::newId()),
+            $name,
+            ToolTier::DIAMOND
+        );
+        $this->efficiency = $efficiency;
+        $this->damage = $damage;
+        $this->durability = $durability;
+
+        $this->initComponent($texture, new CII(CII::CATEGORY_EQUIPMENT, CII::GROUP_PICKAXE));
+        $this->addComponent(new HandEquippedComponent());
+        $this->addComponent(new DamageComponent($this->getAttackPoints()));
+        $this->addComponent(new DurabilityComponent($this->getMaxDurability()));
+    }
+
+    public function getAttackPoints(): int {
+        return $this->damage;
+    }
+
+    public function getMaxDurability(): int {
+        return $this->durability;
+    }
+
+    public function getComponents() : CompoundTag {
+        $itemData = $this->_getComponents();
+        $digger = CompoundTag::create()->setByte("use_efficiency", 1);
+        $destroy_speeds = new ListTag();
+        foreach(DiggerList::getDiggerList($this->getBlockToolType()) as $block){
+            $destroy_speeds->push(CompoundTag::create()
+                ->setString("block", $block)
+                ->setInt("speed", $this->efficiency)
+            );
+        }
+        return $itemData->setTag("components", $itemData->getCompoundTag("components")->setTag("minecraft:digger", $digger->setTag("destroy_speeds", $destroy_speeds)));
+    }
+}
---
+++ a/src/Valres/CustomItemCreator/listeners/ItemConsumeListener.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ *
+ *     ______           __                  ______                 ______                __
+ *   / ____/_  _______/ /_____  ____ ___  /  _/ /____  ____ ___  / ____/_______  ____ _/ /_____  _____
+ *  / /   / / / / ___/ __/ __ \/ __ `__ \ / // __/ _ \/ __ `__ \/ /   / ___/ _ \/ __ `/ __/ __ \/ ___/
+ * / /___/ /_/ (__  ) /_/ /_/ / / / / / // // /_/  __/ / / / / / /___/ /  /  __/ /_/ / /_/ /_/ / /
+ * \____/\__,_/____/\__/\____/_/ /_/ /_/___/\__/\___/_/ /_/ /_/\____/_/   \___/\__,_/\__/\____/_/
+ *
+ * ENG: This file is strictly confidential and personal.
+ * It contains code developed for private purposes and must not be distributed, shared or used without the explicit permission of the author.
+ * Any violation will be subject to legal action.
+ * FRA: Ce fichier est strictement confidentiel et personnel.
+ * Il contient du code développé à des fins privées et ne doit en aucun cas être distribué, partagé ou utilisé sans autorisation explicite de l'auteur.
+ * Toute violation sera passible de poursuites légales.
+ *
+ * @author ValresMC
+ * @version v0.0.1
+ */
+
+declare(strict_types=1);
+
+namespace Valres\CustomItemCreator\listeners;
+
+use pocketmine\entity\effect\EffectInstance;
+use pocketmine\entity\effect\StringToEffectParser;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\item\Item;
+use pocketmine\item\StringToItemParser;
+use pocketmine\player\Player;
+use Valres\CustomItemCreator\items\CustomFood;
+use Valres\CustomItemCreator\managers\FoodEffectsManager;
+
+final class ItemConsumeListener implements Listener
+{
+    public function onEvent(PlayerItemConsumeEvent $event): void {
+        $player = $event->getPlayer();
+        $item = $event->getItem();
+
+        if(!$item instanceof CustomFood) return;
+        $this->addEffects($player, $item);
+    }
+
+    public function addEffects(Player $player, Item $customFood): void {
+        $identifier = StringToItemParser::getInstance()->lookupAliases($customFood)[0];
+        $foodEffects = FoodEffectsManager::getInstance();
+
+        if(array_key_exists($identifier, $foodEffects->getEffects())){
+            $effects = $foodEffects->getEffects()[$identifier];
+
+            foreach($effects as $effectData){
+                [$effectName, $time, $amplifier] = explode(":", $effectData);
+                $player->getEffects()->add(new EffectInstance(StringToEffectParser::getInstance()->parse($effectName), intval($time * 20), intval($amplifier) - 1));
+            }
+        }
+    }
+}
